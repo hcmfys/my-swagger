40a41ee0175d9794832fe0bed5618ae206744d17
fuyousheng <123456>
2020-05-12 16:52:43 +0800

    configure

--- src/main/java/org/springbus/demo/PropConfigureTest.java
+++ src/main/java/org/springbus/demo/PropConfigureTest.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.PropertySources;
 @Configuration
 
 @PropertySources(value = {
-        @PropertySource( value = "classpath:application.properties"),
-        @PropertySource( value = "classpath:application-dev.properties")
+        @PropertySource( value = "classpath:application.properties" ),
+        @PropertySource( value = "classpath:application-dev.properties" )
 
 })
 public class PropConfigureTest {
