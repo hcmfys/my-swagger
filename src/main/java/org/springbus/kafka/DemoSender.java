acf818e1fd00df474c9940d1811ef4d261d24665
fuyousheng <123456>
2020-06-02 09:36:43 +0800

    configure

--- src/main/java/org/springbus/kafka/DemoSender.java
+++ src/main/java/org/springbus/kafka/DemoSender.java
@@ -1,6 +1,7 @@
 package org.springbus.kafka;
 
-import com.oracle.tools.packager.Log;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class DemoSender {
 
     @Autowired
@@ -18,6 +20,6 @@ public class DemoSender {
   public void testDemo() throws InterruptedException {
     kafkaTemplate.send("my-topic", "this is my first demo");
     // 休眠5秒，为了使监听器有足够的时间监听到topic的数据
-    Log.info("====>OK==========");
+    log.info("====>OK==========");
     }
 }
