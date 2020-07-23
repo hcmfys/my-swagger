95210cd1f03b1c50236932c40dea024b6cfe0354
fuyousheng <123456>
2020-07-23 15:11:34 +0800

    configure

--- src/main/java/org/springbus/zdis/RedisSetController.java
+++ src/main/java/org/springbus/zdis/RedisSetController.java
@@ -1,42 +1,143 @@
 package org.springbus.zdis;
 
+import com.alibaba.druid.util.StringUtils;
+import com.google.common.collect.Sets;
+import com.sun.tools.doclets.internal.toolkit.util.ClassUseMapper;
+import io.lettuce.core.*;
+import io.lettuce.core.ScanCursor;
+import org.jooq.meta.derby.sys.Sys;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.util.resources.ga.LocaleNames_ga;
 
-import java.util.List;
-import java.util.Set;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 @RestController
 public class RedisSetController {
     @Autowired
-    private RedisTemplate<String,String>  redisTemplate;
+    private RedisTemplate<String, String> redisTemplate;
 
 
-    @RequestMapping(value = "/addSet",method = RequestMethod.POST)
+    @RequestMapping(value = "/addSet1W", method = RequestMethod.POST)
+    public Set<String> addSet1W(String key) {
+        SetOperations setOperations = redisTemplate.opsForSet();
+        Random r = new Random(System.currentTimeMillis());
+        for (int i = 0; i < 10000; i++) {
+            setOperations.add(key, r.nextInt(10000) + "&index");
+        }
+        return setOperations.members(key);
+    }
+
+    @RequestMapping(value = "/addSet", method = RequestMethod.POST)
     public Set<String> addSet(String key, String value) {
         redisTemplate.opsForSet().add(key, value);
         return redisTemplate.opsForSet().members(key);
     }
 
-    @RequestMapping(value = "/randomMember",method = RequestMethod.POST)
-    public  String  randomMember(String key, String value) {
+    @RequestMapping(value = "/randomMember", method = RequestMethod.POST)
+    public String randomMember(String key, String value) {
 
         return redisTemplate.opsForSet().randomMember(key);
     }
 
-    @RequestMapping(value = "/pop",method = RequestMethod.POST)
-    public  String  pop(String key) {
+    @RequestMapping(value = "/pop", method = RequestMethod.POST)
+    public String pop(String key) {
 
         return redisTemplate.opsForSet().pop(key);
     }
 
-    @RequestMapping(value = "/popCount",method = RequestMethod.POST)
+    @RequestMapping(value = "/popCount", method = RequestMethod.POST)
     public List<String> popCount(String key, long count) {
 
-        return redisTemplate.opsForSet().pop(key,count);
+        return redisTemplate.opsForSet().pop(key, count);
     }
+
+    @RequestMapping(value = "/scan", method = RequestMethod.POST)
+    public List<String> scan(String key, long count) throws IOException {
+        ScanOptions scanOptions = ScanOptions.scanOptions().count(20).build();
+        List<String> myList = new ArrayList<>();
+        Cursor<String> cursor = redisTemplate.opsForSet().scan(key, scanOptions);
+
+        if (cursor.hasNext()) {
+            myList.add(cursor.next() + "---cursorId=" + cursor.getCursorId() + " posId=" + cursor.getPosition());
+
+
+        }
+        cursor.close();
+        return myList;
+    }
+    @RequestMapping(value = "/scanPage", method = RequestMethod.POST)
+    public List<String>  scanPage(String key) throws Exception {
+        ScanOptions scanOptions = ScanOptions.scanOptions().count(10).build();
+
+        List<String> myList = new ArrayList<>();
+        RedisSerializer<String> keySerializer = (RedisSerializer<String>) redisTemplate.getKeySerializer();
+        long curPosId = -1;
+        try (Cursor<byte[]> cursor = redisTemplate.execute((RedisConnection connection) ->
+                connection.sScan(keySerializer.serialize(key),scanOptions), true)) {
+            while (cursor != null && cursor.hasNext()) {
+                long posId = cursor.getCursorId();
+                System.out.println("posId =  " + posId);
+                byte[] rawKey = cursor.next();
+                if (curPosId == -1) {
+                    curPosId = cursor.getCursorId();
+                }
+                if (curPosId != cursor.getCursorId()) {
+                    break;
+                }
+                myList.add(keySerializer.deserialize(rawKey));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return myList;
+    }
+    @RequestMapping(value = "/scanKey", method = RequestMethod.POST)
+    public DataItem<String> scan(String key,String cursorId) {
+        DataItem dataItem = new DataItem();
+        List<String> retList = new ArrayList<>();
+        dataItem.setDataList(retList);
+        if (StringUtils.isEmpty(cursorId)) {
+            cursorId = "0";
+        }
+
+        String destCursorId = cursorId;
+        redisTemplate.execute((RedisCallback<Set<byte[]>>) connection -> {
+            Object obj = connection.getNativeConnection();
+            RedisSerializer<String> keySerializer = (RedisSerializer<String>) redisTemplate.getKeySerializer();
+            AbstractRedisAsyncCommands<byte[], byte[]> commands = (AbstractRedisAsyncCommands) obj;
+            ScanCursor cursor = new ScanCursor(destCursorId, false);
+            ScanArgs scanArgs = ScanArgs.Builder.limit(20);
+            RedisFuture<ValueScanCursor<byte[]>> vv = commands.sscan(keySerializer.serialize(key),
+                    cursor, scanArgs);
+            try {
+                ValueScanCursor<byte[]> result = vv.get();
+
+                for (byte[] bb : result.getValues()) {
+                    retList.add(keySerializer.deserialize(bb));
+                }
+                dataItem.setCursorId(result.getCursor());
+                System.out.println(cursor);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        });
+
+        return dataItem;
+    }
+
 
 }
