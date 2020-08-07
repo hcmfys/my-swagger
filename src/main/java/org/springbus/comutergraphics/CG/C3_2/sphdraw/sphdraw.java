c9af01b84255f1596f11bac79bccda69ae9c6e29
fuyousheng <123456>
2020-08-07 15:58:17 +0800

    configure

--- src/main/java/org/springbus/comutergraphics/CG/C3_2/sphdraw/sphdraw.java
+++ src/main/java/org/springbus/comutergraphics/CG/C3_2/sphdraw/sphdraw.java
@@ -1,15 +1,8 @@
 package org.springbus.comutergraphics.CG.C3_2.sphdraw;
-// 本ファイルの著作権は、株式会社オーム社および本書の著作者である青野雅樹
-// および日本アイビーエム（株）に帰属します。
-// 本ファイルを利用したことによる直接あるいは間接的な損害に関して、
-// 著作者およびオーム社はいっさいの責任を負いかねますので、
-// あらかじめご了承ください
-// また，本ファイルを他のウェブサイトで公開すること，およびCD-ROMなどの
-// ディジタルメディアで再配布すること，ならびに販売目的で使用することは
-// お断りします。
 
-// sphdraw.java (球の線画表示プログラム：アプレット）
-//	プログラム３−１９
+
+// sphdraw.java（球面线显示程序：applet）
+//程序3-19
 
 import org.springbus.comutergraphics.CG.common.*;
 
@@ -17,51 +10,51 @@ import java.awt.*;
 
 public class sphdraw extends JApplet{
 
-	ObjectWorld ow; //ObjectWorldクラスの変数
-	MyCanvas m; // MyCanvasクラスの変数
-	double screenx, screeny;// スクリーンサイズ
+	ObjectWorld ow; //ObjectWorld类的变量
+	MyCanvas m; // MyCanvas类的变量
+	double screenx, screeny;// 屏幕尺寸
 
 	public   void init(){
 
-		// ObjectWorldクラスのオブジェクト生成
-		ow = new ObjectWorld(); 
-		// 世界（ユニバース）の生成
+		// ObjectWorld类对象生成
+		ow = new ObjectWorld();
+		// 世界的一代（宇宙）
 		ObjectNode universe = ow.createUniverse();
-		// MyCanvasクラスのオブジェクト生成
+		// MyCanvas类的对象创建
 		m = new MyCanvas(this);
-		// Cameraクラスのオブジェクト生成
+		// 相机类对象生成
 		Camera cam = new Camera(m);
-		// スクリーンサイズの設定
+		//屏幕尺寸设定
 		screenx = cam.getScreenX(); screeny = cam.getScreenY();
-		// ウィンドウの設定
+		// 视窗设定
 		m.setWindow(-screenx/2,screenx/2,-screeny/2,screeny/2);
-		// オフスクリーンバッファの設定
+		// 屏幕外缓冲区设置
 		m.setOffScreenBuffer();
-		// カメラの位置の設定
-		cam.setEyePosition(0,0,2); 
-		// カメラの回転角度設定（X軸の周りに0.3ラジアン回す）
+		// 设定相机位置
+		cam.setEyePosition(0,0,2);
+		// 相机旋转角度设置（绕X轴旋转0.3弧度）
 		cam.rotate(1,0,0,0.3);
-		// カメラを世界に追加
+		// 向世界添加相机
 		ow.addCamera(cam,"CAMERA1");
-		// 球の定義
+		// 球体的定义
 		Sphere sph = new Sphere(1);
-		// 球をシーングラフに追加
+		// 将球体添加到场景图
 		ObjectNode sphNode = universe.addChild(sph,"Sphere");
 
 	}
 
   public void paint(Graphics g) {
-    if (m != null) {
-      // オフスクリーンを設定
-      m.setGraphics(MyCanvas.OFFSCREEN_GRAPHICS);
-      // オフスクリーンで球を描画
-      ow.drawWorld();
-      // フロントスクリーンに変更
-      m.setGraphics(MyCanvas.DEFAULT_GRAPHICS);
-      // オフスクリーン画像をフロントスクリーンに描画
-      m.drawImage(m.getOffScreenImage(), -screenx / 2, screeny / 2, this);
-    }
-	}
+	  if (m != null) {
+		  // 设置萤幕
+		  m.setGraphics(MyCanvas.OFFSCREEN_GRAPHICS);
+		  //在屏幕外绘制球体
+		  ow.drawWorld();
+		  // 切换到前屏
+		  m.setGraphics(MyCanvas.DEFAULT_GRAPHICS);
+		  //在前屏幕上绘制屏幕外图像
+		  m.drawImage(m.getOffScreenImage(), -screenx / 2, screeny / 2, this);
+	  }
+  }
 
 	public  static void  main(String[] args) {
 		new sphdraw();
