f23b8c153ecac055a4bc9ace5ea07e904375a754
fuyousheng <123456>
2020-08-17 11:57:54 +0800

    configure

--- src/main/java/org/springbus/adt/BuddleSort.java
+++ src/main/java/org/springbus/adt/BuddleSort.java
@@ -3,6 +3,13 @@ package org.springbus.adt;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * 桶排序
+ * 原理：假设输入数据服从均匀分布，将数据分到有限数量的桶里，
+ * 每个桶再分别排序（有可能再使用别的排序算法或是以递归方式继续使用桶排序进行排）
+ * 具体操作： 设置一个定量的数组当作空桶；遍历输入数据，并且把数据一个一个放到对应的桶里去；
+ * 对每个不是空的桶进行排序；从不是空的桶里把排好序的数据拼接起来。
+ */
 public class BuddleSort {
 
     public  static  void main(String[] args) {
